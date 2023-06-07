115ae4150e3f000c9e18be278f03b00c0d2f2a31
RidgeWada <rwada@revacomm.com>
2023-06-07 10:42:49 -1000

    Wip Query Async Test

--- test/prom-query-tests/UnitTest1.cs
+++ test/prom-query-tests/UnitTest1.cs
@@ -1,10 +1,25 @@
 namespace prom_query_tests;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+using PrometheusQuerySdk;
+using PrometheusQuerySdk.Models;
 
-    }
+public class UnitTest1 {
+  [Fact]
+  public async void QueryAsync() {
+    using var httpClient = CreateHttpClient();
+    var rwClient = new PrometheusRemoteProtocolClient(httpClient);
+    using var source = new CancellationTokenSource();
+    var token = source.Token;
+    var queryClient = new PrometheusClient(CreateHttpClient);
+    var timestamp = DateTime.UtcNow;
+    var data = new QueryPostRequest("test",timestamp,null);
+    var result = await queryClient.QueryPostAsync(data,token);
+    Assert.Equals(data.Timestamp,result.Data.Result[0].Value);
+  }
+
+  private static HttpClient CreateHttpClient() {
+    var httpClient = new HttpClient();
+    httpClient.BaseAddress = new Uri("http://localhost:9091/");
+    return httpClient;
+  }
 }

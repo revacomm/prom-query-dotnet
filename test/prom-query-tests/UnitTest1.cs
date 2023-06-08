abc47430a527f35ad363beec57c0b277b21ee161
btakushi <blaise.takushi@cms.hhs.gov>
2023-06-08 09:45:39 -1000

    use remoteWrite for pushing test data

--- test/prom-query-tests/UnitTest1.cs
+++ test/prom-query-tests/UnitTest1.cs
@@ -1,20 +1,59 @@
+using Prometheus;
+using Xunit.Abstractions;
+
 namespace prom_query_tests;
 
 using PrometheusQuerySdk;
 using PrometheusQuerySdk.Models;
 
 public class UnitTest1 {
+  private readonly ITestOutputHelper output;
+
+  public UnitTest1(ITestOutputHelper outputHelper) {
+    this.output = outputHelper;
+  }
   [Fact]
   public async void QueryAsync() {
+    // initial setup
     using var httpClient = CreateHttpClient();
-    var rwClient = new PrometheusRemoteProtocolClient(httpClient);
     using var source = new CancellationTokenSource();
     var token = source.Token;
-    var queryClient = new PrometheusClient(CreateHttpClient);
+    var rwClient = new PrometheusRemoteProtocolClient(httpClient);
+
+    // timestamp configuration
     var timestamp = DateTime.UtcNow;
+    DateTimeOffset dto = new DateTimeOffset(timestamp);
+
+    // label and value
+    var query = "testData";
+    var value = 1.0;
+
+    // push test data to prometheus
+    var writeRequest = new WriteRequest {
+      Timeseries = {
+        new TimeSeries {
+          Labels = {
+            new Label { Name = query }
+          },
+          Samples = {
+            new Sample {
+              Timestamp = dto.ToUnixTimeMilliseconds(),
+              Value = value
+            }
+          }
+        }
+      }
+    };
+    await rwClient.WriteAsync(writeRequest, token);
+
+    // query test data
+    var queryClient = new PrometheusClient(CreateHttpClient);
+
     var data = new QueryPostRequest("test",timestamp,null);
     var result = await queryClient.QueryPostAsync(data,token);
-    Assert.Equals(data.Timestamp,result.Data.Result[0].Value);
+    var resultLabel = result.Data.Result[0].Labels.FirstOrDefault().Key;
+    this.output.WriteLine(resultLabel);
+    Assert.Equal(query,resultLabel);
   }
 
   private static HttpClient CreateHttpClient() {

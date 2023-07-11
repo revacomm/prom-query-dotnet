bd84b2c25608f2585ac2b9e92a279dd451adff96
RidgeWada <rwada@revacomm.com>
2023-07-11 11:36:10 -1000

    finished label value method

--- test/prom-query-tests/MetadataQueriesIntegrationTests.cs
+++ test/prom-query-tests/MetadataQueriesIntegrationTests.cs
@@ -48,6 +48,22 @@ public class MetadataQueriesIntegrationTests {
      });
    }
 
+  // Test Label Value Async
+  [Fact]
+  public async void TestLabelValueAsync() {
+    await this.LabelValueTestHelper((queryClient, labelKey, labels, token) => {
+      return queryClient.LabelValueAsync(labelKey, labels, null, null, token);
+    });
+  }
+
+  // Test Label Value Post Async
+  [Fact]
+  public async void TestLabelValuePostAsync() {
+    await this.LabelValueTestHelper((queryClient, labelKey, labels, token) => {
+      return queryClient.LabelValuePostAsync(labelKey, labels, null, null, token);
+    });
+  }
+
   private async Task LabelsTestHelper(Func<PrometheusClient, String[]?, CancellationToken, Task<ResponseEnvelope<IImmutableList<String>>>> runLabelQuery) {
     using var source = new CancellationTokenSource();
     var token = source.Token;
@@ -145,6 +161,67 @@ public class MetadataQueriesIntegrationTests {
     Assert.NotNull(outOfRangeLabelQueryRangeResult.Data);
     Assert.DoesNotContain(testLabel1.Name, outOfRangeLabelQueryRangeResult.Data); // Tests that testLabel1's name is not within the range
     Assert.DoesNotContain(testLabel2.Name, outOfRangeLabelQueryRangeResult.Data); // Tests that testLabel2's name is not within the range
+  }
+
+  private async Task LabelValueTestHelper(Func<PrometheusClient, String, String[]?, CancellationToken, Task<ResponseEnvelope<IImmutableList<String>>>> runLabelValueQuery) {
+    using var source = new CancellationTokenSource();
+    var token = source.Token;
+    var queryClient = new PrometheusClient(testHelper.CreateHttpClient);
+    var seed = DateTime.UtcNow;
+    var lastSampleTime = seed.Subtract(TimeSpan.FromMinutes(1));
+
+    // create timseries samples
+    var samples = testHelper.CreateTestData(lastSampleTime, 4);
+
+    // create test labels
+    var testLabel1 = CreateTestLabel();
+    var testLabel2 = CreateTestLabel();
+
+    // create target label name
+    var targetLabelName = testLabel1.Name;
+
+    // push test data in 2 rounds, 1st with label 1 and the 2nd with
+    // label 2
+    var labelArg1 = new Google.Protobuf.Collections.RepeatedField<Label>();
+    labelArg1.AddRange(new[] {testLabel1});
+
+    var labelArg2 = new Google.Protobuf.Collections.RepeatedField<Label>();
+    labelArg2.AddRange(new[] {testLabel2});
+
+    // creates two different series the 1st with testLabel1 and 2nd with testLabel2
+    var query1Series = await testHelper.PushTestData(samples, labelArg1, token);
+    var query2Series = await testHelper.PushTestData(samples, labelArg2, token);
+
+    // *Note* This test is only dealing with Label Values
+    // In this test, the target name is testLabel1's name and the series is
+    // query1Series which contains only testLabel 1's name and value
+    // the result should contain only testLabel1's Value and not testLabel2's value
+    // because only testLabel1's Value matches with the target name and is within the series
+    var labelQueryResult = await runLabelValueQuery(
+      queryClient,
+      targetLabelName,
+      new String[] {query1Series},
+      token
+    );
+    Assert.NotNull(labelQueryResult.Data);
+    Assert.Contains(testLabel1.Value,labelQueryResult.Data); // Tests if testLabel1's value is within the data
+    Assert.DoesNotContain(testLabel2.Value,labelQueryResult.Data); // Tests if testLabel2's value is not within the data
+
+    // *Note* This test is only dealing with Label Values
+    // In this test, the target name is also testLabel1's name, but the series is
+    // query2Series which contains only testLabel 2's name and value
+    // the result should contain no values because
+    // testLabel 1's value is not within the series being tested, therefore no
+    // value is created for it and testLabel 2's value does not match with the target name
+    var labelQueryResult2 = await runLabelValueQuery(
+      queryClient,
+      targetLabelName,
+      new String[] {query2Series},
+      token
+    );
+    Assert.NotNull(labelQueryResult2.Data);
+    Assert.DoesNotContain(testLabel1.Value,labelQueryResult2.Data); // Tests if testLabel1's value is not within the data
+    Assert.DoesNotContain(testLabel2.Value, labelQueryResult2.Data); // Tests if testLabel2's value is not within the data
   }
 
   // Creates random test label
